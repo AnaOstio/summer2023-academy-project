99cdb3a44076cd8127de79191c70731b30df3123
Ana Fernández Ostio <anafdezostio01@gmail.com>
2023-06-08 10:45:57 -0700

    Initial commit

---
+++ src/main/java/co/empathy/academy/summer2023academyproject/Summer2023AcademyProjectApplication.java
@@ -0,0 +1,13 @@
+package co.empathy.academy.summer2023academyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Summer2023AcademyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Summer2023AcademyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/empathy/academy/summer2023academyproject/Summer2023AcademyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package co.empathy.academy.summer2023academyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Summer2023AcademyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
